d9d4ef456abad519c68f40b2478a2929bf64ecaa
jack.yan <szjack@163.com>
2018-06-21 16:25:31 +0800

    1111

--- src/main/java/com/devops/config/PathGitHubConstant.java
+++ src/main/java/com/devops/config/PathGitHubConstant.java
@@ -2,21 +2,21 @@ package com.devops.config;
 
 public class PathGitHubConstant {
 	/**
-	 * GIT  GITEE path CONFIG
+	 * GIT  GITHUB path CONFIG
 	 */
 	public static String  GIT_USER_NAME="szjack";
 	public static String  GIT_LOCAL_PATH_BASE="/home/git/";
 	public static String  GIT_SSH_PATH="~/.ssh/config";
-	public static String  GIT_GITEE_URL="https://gitee.com/";
-	public static String  GIT_GITEE_AUTHOR_PRE="git@gitee.com";
+	public static String  GIT_GITHUB_URL="https://GITHUB.com/";
+	public static String  GIT_GITHUB_AUTHOR_PRE="git@GITHUB.com";
 	
 	/**
 	 * COMMON GIT PATH
 	 */
-	public static String GIT_URL=GIT_GITEE_URL;
-	public static String  GIT_AUTHOR_PRE=GIT_GITEE_AUTHOR_PRE;
+	public static String GIT_URL=GIT_GITHUB_URL;
+	public static String  GIT_AUTHOR_PRE=GIT_GITHUB_AUTHOR_PRE;
 	public static String  GIT_URI=GIT_URL+GIT_USER_NAME+"/";
-	public static String  GIT_GITEE_AUTHOR_USER=GIT_AUTHOR_PRE+":"+GIT_USER_NAME+"/";
+	public static String  GIT_GITHUB_AUTHOR_USER=GIT_AUTHOR_PRE+":"+GIT_USER_NAME+"/";
 	
 	/**
 	 * JENKINS path CONFIG
@@ -32,18 +32,18 @@ public class PathGitHubConstant {
 	/**
 	 * TOKEN CONFIG
 	 */
-	public static String TOKEN_GITEE="5e9f2eaf9a572b54af3d6607e42a08b3";
+	public static String TOKEN_GITHUB="5e9f2eaf9a572b54af3d6607e42a08b3";
 	/**
 	 *  API URL path CONFIG
 	 */
-	public static String API_GITEE_CREATE_REPO="https://gitee.com/api/v5/user/repos";
+	public static String API_GITHUB_CREATE_REPO="https://GITHUB.com/api/v5/user/repos";
 	
-	public static String API_GITEE_DELETE_REPO="https://gitee.com/api/v5/repos/"+GIT_USER_NAME+"/";
+	public static String API_GITHUB_DELETE_REPO="https://GITHUB.com/api/v5/repos/"+GIT_USER_NAME+"/";
 	
 	/**
 	 * COMMON API 
 	 */
-	public  static String API_CREATE_REPO=API_GITEE_CREATE_REPO;
-	public  static String API_DELETE_REPO=API_GITEE_DELETE_REPO;
+	public  static String API_CREATE_REPO=API_GITHUB_CREATE_REPO;
+	public  static String API_DELETE_REPO=API_GITHUB_DELETE_REPO;
 }
 
